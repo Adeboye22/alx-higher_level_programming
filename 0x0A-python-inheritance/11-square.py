c9259bfc5f82f1cb1692ef4536474ff23c35f709
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-07-05 03:16:28 +0100

    python inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
-Square = __import__('11-square').Square
+"""
+    11-square: class Square from Rectangle
+"""
 
-s = Square("erroR")
 
-print(s)
-print(s.area())
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        Square  inherits from Rectangle
+        Attributes:
+            size (int): side of square
+        Methods:
+            __init__ - initialises the square
+    """
+    def __init__(self, size):
+        """
+            initialises Square
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """
+            Returns the area of square
+        """
+        area = self.__size * self.__size
+        return area
+
+    def __str__(self):
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                    self.__size, self.__size))

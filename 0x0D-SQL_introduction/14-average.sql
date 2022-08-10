a46c3452eaae08d8a03f411d7fdd5b11d5587d92
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-08-10 04:12:25 +0100

    updated

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
--- a script that removes all records with a score <= 5 in the table second_table
-DELETE FROM second_table WHERE score <= 5;
-
+-- a script that computes the score average of all records in the table second_table
+SELECT AVG(score) AS average FROM second_table;
